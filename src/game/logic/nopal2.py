a7d4cf34f63d37b1e36b14d3acf41a8867524514
NopalAul <mnaufal0605@gmail.com>
2024-02-27 09:02:29 +0700

    fix nopal2 (greedy by diamond)

--- src/game/logic/nopal2.py
+++ src/game/logic/nopal2.py
@@ -6,6 +6,13 @@ from game.models import GameObject, Board, Position
 from ..util import get_direction
 
 class NopalLogic2(BaseLogic):
+    # GREEDY BY DIAMOND
+    # akurat cari diamond terdekat
+    # kalau inventori udah 4, ketemu diamond point 2 (4 + 2 != 5), maka skip diamond tersebut (cegah error)
+    # timing base: detik 46 kalau masih jauh dari base, suruh pulang, kalau dekat dengan base, suruh pulang di detik 53
+    # tackle: NONE
+    # defense: NONE
+
     def __init__(self):
         self.timer_to_base = 0
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -14,6 +21,7 @@ class NopalLogic2(BaseLogic):
 
 
     def distance(self, x2, x1, y2, y1):
+        # hitung jarak antara dua titik
         return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
     
@@ -21,46 +29,40 @@ class NopalLogic2(BaseLogic):
         props = board_bot.properties
         base = board_bot.properties.base
 
-        # distance_to_base = self.distance(base.x, board_bot.position.x, base.y, board_bot.position.y)
-        # print(f'BASE DIST: {distance_to_base}')
-        # too_far = False
-        # print(f'too far?: {too_far}')
-        # if distance_to_base > 5:
-        #     too_far = True
-
+        # Monitor jarak bot ke base
+        distance_to_base = self.distance(base.x, board_bot.position.x, base.y, board_bot.position.y)
+        print(f'BASE DIST: {distance_to_base}')
+        
+        # Pulang ketika inventory penuh
         if props.diamonds == 5:
-            # Move to base
             self.goal_position = base # Base(y=10, x=3)
             
-        
-        # elif self.timer_to_base >= 46 and too_far:
+        # Base timing management
         elif self.timer_to_base >= 46:
-            print("timer hit")
-            self.goal_position = base
+            if distance_to_base > 5:
+                print("TOO FAR")
+                self.goal_position = base
+            elif self.timer_to_base >= 53:
+                print("timer hit")
+                self.goal_position = base
+            else:
+                print("BELUM WAKTUNYA BALIK")
 
         else:
-            # Just roam around
-            # self.goal_position = None
-
+            # Inisiasi posisi diamond pertama
             diamond_position = board.diamonds[0].position
             self.goal_position = diamond_position
             print("POSISI diaomon: ")
             print(diamond_position)
 
-            # diamond terdekat dari bot
+            # Cari diamond terdekat dari bot
             print(f'DIAMOND COUNT: {len(board.diamonds)}')
-            # for i in range(1, len(board.diamonds)):
-            #     print(f'ALL DIAMOND: {board.diamonds[i].position}')
-            #     if (board.diamonds[i].position.x - board_bot.position.x) + (board.diamonds[i].position.y - board_bot.position.y) < (diamond_position.x - board_bot.position.x) + (diamond_position.y - board_bot.position.y):
-            #         diamond_position = board.diamonds[i].position
-            #         self.goal_position = diamond_position
-            #         diamond_position_new = diamond_position
-            # print("POSISI diaomon NEW: ")
-            # print(diamond_position_new)
-
             for i in range(1, len(board.diamonds)):
                 print(f'ALL DIAMOND: {board.diamonds[i].position}')
                 if self.distance(board.diamonds[i].position.x, board_bot.position.x, board.diamonds[i].position.y, board_bot.position.y) < self.distance(diamond_position.x, board_bot.position.x, diamond_position.y, board_bot.position.y):
+                    if board.diamonds[i].properties.points == 2 and props.diamonds == 4:
+                        print("DIAMOND 2 TAPI UDAH 4")
+                        continue
                     diamond_position = board.diamonds[i].position
                     self.goal_position = diamond_position
                     diamond_position_new = diamond_position
@@ -80,15 +82,15 @@ class NopalLogic2(BaseLogic):
                 self.goal_position.y,
             )
 
-            if delta_x == delta_y:
-                print("X SAMA Y SAMA")
-                step = random.randint(-1, 1)
-                delta_x = step
-                if delta_y == delta_x:
-                    if delta_x == 0:
-                        delta_y = 1
-                    else :
-                        delta_y = delta_x*-1
+            # if delta_x == delta_y:
+            #     print("X SAMA Y SAMA")
+            #     step = random.randint(-1, 1)
+            #     delta_x = step
+            #     if delta_y == delta_x:
+            #         if delta_x == 0:
+            #             delta_y = 1
+            #         else :
+            #             delta_y = delta_x*-1
 
         else:
             # Roam around
